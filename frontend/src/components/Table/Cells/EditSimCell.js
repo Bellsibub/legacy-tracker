71fa3e3b553f1ee875321224bde8ef527bc043ea
Bellsibub <isabella.billgren@gmail.com>
2021-06-14 16:57:07 +0200

    edit sim in gen list

--- frontend/src/components/Table/Cells/EditSimCell.js
+++ frontend/src/components/Table/Cells/EditSimCell.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import _ from 'lodash';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 
 // @material-ui/core components
@@ -8,8 +8,8 @@ import TableCell from '@material-ui/core/TableCell';
 
 import DialogSims from 'components/DialogSims';
 
-// actions
-import { editSim } from 'store/legacy';
+// services
+import { updateSim } from 'store/legacy/services';
 
 import styles from '../style';
 
@@ -18,9 +18,11 @@ const useStyles = makeStyles(styles);
 export default ({ item }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const { _id } = useSelector((store) => store.legacy);
 
   const handleEditSimConfirm = (newSim) => {
-    dispatch(editSim({ ...newSim }));
+    console.log('NEW sim to update', newSim);
+    dispatch(updateSim({ simData: newSim, legacyID: _id }));
   };
 
   return (
