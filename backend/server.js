5de597a647a7c5c72cfd3ee069ae3f066845030a
Bellsibub <isabella.billgren@gmail.com>
2021-06-17 19:33:46 +0200

    auth stage1 added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import jwks from 'jwks-rsa';
+import jwt from 'express-jwt';
 
 import router from './routes';
 import errorHandler from './controllers/errorController';
@@ -27,9 +29,22 @@ if (process.env.RESET_DB) {
 const port = process.env.PORT || 8080;
 const app = express();
 
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://legacytracker.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://localhost:3004',
+  issuer: 'https://legacytracker.eu.auth0.com/',
+  algorithms: ['RS256']
+});
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use(jwtCheck);
 
 // Add router module and error handler
 app.use('/', router);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,17 +10,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import store from 'store';
 import HomeLayout from 'pages/Home';
 import NoMatch from 'pages/nomatch';
+import { BASE_URL, BASE_AUTH_URL } from 'utils/apiConfig'
 
 import { muitheme } from 'assets/jss/theme';
 
 const providerConfig = {
   domain: 'legacytracker.eu.auth0.com',
   clientId: 'WkUQW6mwuCCxzLSIcDv3lBsEu5AgtsnQ',
-  // ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: `${window.location.origin}/dashboard`
-  // onRedirectCallback,
+  audience: BASE_URL,
+  redirectUri: `${window.location.origin}/dashboard`,
+  scope: 'read:current_user'
 };
-
 ReactDOM.render(
   <Auth0Provider {...providerConfig}>
     <Provider store={store}>
